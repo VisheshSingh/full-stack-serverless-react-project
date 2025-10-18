461e555d8e94a813867ed54eb6bbc0a0da078992
Vishesh Singh <visheshsingh16@gmail.com>
2025-10-18 15:25:00 -0400

    🦾 restructure folder  + more handler functions

---
+++ backend/handlers/get.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+  console.log({ event });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message:
+        'Go Serverless v4! Your function executed successfully! - checking multi-stage deployment',
+    }),
+  };
+};
